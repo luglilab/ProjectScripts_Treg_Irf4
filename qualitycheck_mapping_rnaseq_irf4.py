97d0165f0eec882de34d076128d48a844ce51d38
sinnamone <sinnamone@gmail.com>
2019-01-17 16:56:56 +0100

    Fustq-dump added

--- qualitycheck_mapping_rnaseq_irf4.py
+++ qualitycheck_mapping_rnaseq_irf4.py
@@ -1,8 +1,9 @@
 import sys
 import subprocess
 import os
+import traceback
 # set path tools used
-paralleldump = "/home/spuccio/miniconda3/envs/rnaseq_env/bin/parallel-fastq-dump"
+fastqdump = "/home/spuccio/miniconda3/envs/rnaseq_env/bin/fastq-dump" # version 0.6.3
 #
 thread = 40
 rawdatadir = "/mnt/datadisk2/spuccio/SP011_Integration_ChipSeqGSE98264_RnaSeqSP010/raw_data_Nature_Imm_GSE49929"
@@ -18,9 +19,15 @@ else:
 # move in raw read folder
 os.chdir(rawdatadir)
 # download from sra row data
-
 for i in range(len(sraid)):
     try:
-        subprocess.check_call("".join([paralleldump,"--sra-id",sraid[i],"--threads",str(thread),"--outdir",rawdatadir]),
-                              shell=True)
+        subprocess.check_call(" ".join([fastqdump,"-I","--split-files",sraid[i],"-O",rawdatadir]),shell=True)
+    except (subprocess.CalledProcessError,traceback), e:
+        print "Fastq-Dump process fail. Exit"
+        sys.exit(1)
+    else:
+        print "Fastq-Dump process complete."
+#rename file
+
+
 
