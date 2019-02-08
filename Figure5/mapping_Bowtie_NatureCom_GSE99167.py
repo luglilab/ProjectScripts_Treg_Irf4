b06d797fc75c1213f9c96923e4f92b09fe4b129a
sinnamone <sinnamone@gmail.com>
2019-02-08 19:15:29 +0100

    repeat heatmap

---
+++ Figure5/mapping_Bowtie_NatureCom_GSE99167.py
@@ -0,0 +1,84 @@
+import os
+import subprocess
+import sys
+# set path tool
+threads = "40"
+MAX_FRAG_LEN = "2000"
+# the multimapping flag
+multimap = "4"
+# folder with Mus musculus (house mouse) genome assembly GRCm38 (mm10)
+GRCm38indexpath = "/home/spuccio/AnnotationBowtie2/Mus_musculus/GRCm38.p6"
+# bowtie2-build path
+bowtiebuild2path = "/home/spuccio/miniconda3/envs/chipseq_env/bin/bowtie2-build"
+# bowtie2 path
+bowtie2path = "/home/spuccio/miniconda3/envs/chipseq_env/bin/bowtie2"
+# path folder
+projectdir = "/mnt/datadisk2/spuccio/SP011_Integration_ChipSeqGSE98264_RnaSeqSP010/"
+# path out file sam
+mappingout = "".join([projectdir, "mapping"])
+# input file path
+raw_data_dir = "".join([projectdir, "Raw_ReadsChipFigure5/"])
+#
+GRCm38fasta = "".join([GRCm38indexpath, "/GRCm38.primary_assembly.fa"])
+raw_fastq = {"Th9_BATF_r1": ["Th9_BATF_input_1.fastq", "Th9_BATF_input_2.fastq"],
+             "Th9_BATF_r2": ["Th9_BATF_input_3.fastq","Th9_BATF_input_4.fastq"],
+             "Th9_BATF_r3": ["Th9_BATF_input_5.fastq", "Th9_BATF_input_6.fastq"],
+             "Th9_BATF_r4": ["Th9_BATF_input_7.fastq", "Th9_BATF_input_8.fastq"],
+             "Th9_WT_r1": ["Th9_BATF_input_1.fastq", "Th9_BATF_input_2.fastq"],
+             "Th9_WT_r2": ["Th9_BATF_input_3.fastq","Th9_BATF_input_4.fastq"],
+             "Th9_WT_r3": ["Th9_BATF_input_5.fastq", "Th9_BATF_input_6.fastq"],
+             "Th9_WT_r4": ["Th9_BATF_input_7.fastq", "Th9_BATF_input_8.fastq"]
+             }
+
+
+def createdir(dirpath):
+    if not os.path.exists(dirpath):
+        os.mkdir(dirpath)
+        print(" ".join(["Directory", dirpath.split("/")[-1], "Created"]))
+    else:
+        print(" ".join(["Directory", dirpath.split("/")[-1], "already exists"]))
+
+# check index function
+
+
+def checkindex(indexpath, fastafile, indexname):
+    for i in range(len(os.listdir(indexpath))):
+        if os.listdir(indexpath)[i].split(".")[-1] == "bt2":
+            print("Genome index of Fasta file already exists.")
+            break
+        else:
+            try:
+                os.chdir(GRCm38indexpath)
+                subprocess.check_call(" ".join([bowtiebuild2path, "--threads", threads, fastafile, indexname]),
+                                      shell=True)
+            except subprocess.CalledProcessError:
+                print("ERROR.Fastqc analysis failed. Stop execution.")
+                sys.exit(1)
+            else:
+                print("Fastq analysis complete.")
+    return indexname
+
+
+def bowtie2mappingpairedend(indexname, fastqname, samname):
+    try:
+        subprocess.check_call(" ".join([bowtie2path, "-p", threads, "-q", "--local", "-k", multimap,
+                                        "-x", indexname, "-X", MAX_FRAG_LEN,
+                                        "-1", "".join([raw_data_dir, fastqname[0]]),
+                                        "-2", "".join([raw_data_dir, fastqname[1]]),
+                                        "-S", "".join([mappingout, samname])]),
+                              shell=True)
+    except subprocess.CalledProcessError:
+        print("ERROR.Mapping of %s with bowtie2 failed. Stop execution." % fastqname)
+        sys.exit(1)
+    else:
+        print("Mapping of %s with bowtie2 complete." % fastqname)
+
+
+
+
+if __name__ == "__main__":
+    index = checkindex(GRCm38indexpath, GRCm38fasta, "GRCm38")
+    createdir(mappingout)
+    os.chdir(raw_data_dir)
+    for key, value in raw_fastq.items():
+        bowtie2mappingpairedend("".join([GRCm38indexpath, "/", index]), value, key)
--- pscan_analysis.py
+++ pscan_analysis.py
@@ -8,6 +8,7 @@ from pybiomart import Server
 from Bio import SeqIO
 import subprocess
 #
+pscanpath = "/home/spuccio/pscan/pscan"
 server = Server(host='http://www.ensembl.org')
 dataset = (server.marts['ENSEMBL_MART_ENSEMBL'].datasets['hsapiens_gene_ensembl'])
 conversionscript = "/mnt/datadisk2/spuccio/SP011_Integration_ChipSeqGSE98264_RnaSeqSP010/ProjectScripts/utility.py"
@@ -48,6 +49,7 @@ print "The number of DEGS with positive log2Fold change after conversion is %d "
 print "The number of DEGS with negative log2Fold change after conversion is %d " % len(dfDOWN_converted)
 # filter Jaspar Database with RNA-Seq Expressed Genes
 # import expressed genes
+print "Parsing Background"
 dfExpressed = pd.read_csv("".join([EdgeResultsfolder,expressed]),sep="\t",header=0)
 # filter non expressed genes
 dfExpressed = dfExpressed[dfExpressed[ u'pvalue'].notnull()].reset_index(drop=True)
@@ -61,7 +63,7 @@ dfExpressedConverted = dfExpressedConverted.reset_index()
 dfExpressedConverted = dfExpressedConverted[['Id','RefSeq mRNA ID']]
 # converte fasta to tab
 try:
-    subprocess(" ".join(["python",conversionscript,"-i", fastapromotor, "-o", "Homo_sapiens_950up_50down", "-p", OutputPAth, "-t", "fasta2tab", "-r 1"]),shell=True)
+    subprocess.check_call(" ".join(["python",conversionscript,"-i", fastapromotor, "-o", "Homo_sapiens_950up_50down", "-p", OutputPAth, "-t", "fasta2tab", "-r 1"]),shell=True)
 except:
     print "Complete"
 else:
@@ -75,14 +77,82 @@ dfPromoterTab['RefseqId'] = dfPromoterTab[['prefix', 'RefseqGenePromotor']].appl
 # delete temp columns
 dfPromoterTab = dfPromoterTab.drop(['assembly','consortium','prefix','RefseqGenePromotor'], axis=1)
 # create tab filtered for expressed
+
 dfpromoter_expressed = pd.merge(dfExpressedConverted,dfPromoterTab,left_on="RefSeq mRNA ID",right_on="RefseqId")
+dfpromoter_expressed_in = pd.merge(dfExpressedConverted,dfPromoterTab,left_on="RefSeq mRNA ID",right_on="RefseqId")
 dfpromoter_expressed = dfpromoter_expressed[[0,1]]
+dfpromoter_expressed[0] = dfpromoter_expressed[0].str.replace('>','')
 dfpromoter_expressed.to_csv(OutputPAth+"promoter_expressed.tab",sep="\t",header=False,index=False)
-subprocess(" ".join(["python",conversionscript,"-i", OutputPAth+"promoter_expressed.tab", "-o", "Homo_sapiens_950up_50down_promoter_expressed", "-p", OutputPAth, "-t", "tab2fasta"]),shell=True)
 try:
-    subprocess(" ".join(["python",conversionscript,"-i", OutputPAth+"promoter_expressed.tab", "-o", "Homo_sapiens_950up_50down_promoter_expressed", "-p", OutputPAth, "-t", "tab2fasta"]),shell=True)
+    subprocess.check_call(" ".join(["python", conversionscript, "-i", OutputPAth + "promoter_expressed.tab", "-o",
+                                    "Homo_sapiens_950up_50down_promoter_expressed", "-p", OutputPAth, "-t",
+                                    "tab2fasta"]), shell=True)
 except:
-    print "Complete"
+    print "Error during background parsing."
 else:
-    print "Complete"
-print dfPromoterTab.head()
+    print "Background parsing complete."
+print "Parsing Input Promoter from DEGS upregulated"
+promoter_seq_up =  pd.merge(dfUP_converted,dfpromoter_expressed_in,left_on="RefSeq mRNA ID",right_on="RefseqId")
+promoter_seq_down =  pd.merge(dfDOWN_converted,dfpromoter_expressed_in,left_on="RefSeq mRNA ID",right_on="RefseqId")
+# subset columns
+promoter_seq_up = promoter_seq_up[[0,1]]
+promoter_seq_down = promoter_seq_down[[0,1]]
+# sed >
+promoter_seq_up[0] = promoter_seq_up[0].str.replace('>','')
+promoter_seq_down[0] = promoter_seq_down[0].str.replace('>','')
+# export
+promoter_seq_up.to_csv(OutputPAth+"promoter_DEGS_up.tab",sep="\t",header=False,index=False)
+promoter_seq_down.to_csv(OutputPAth+"promoter_DEGS_down.tab",sep="\t",header=False,index=False)
+# tab2fasta up
+try:
+    subprocess.check_call(" ".join(["python", conversionscript, "-i", OutputPAth + "promoter_DEGS_up.tab", "-o",
+                                    "Homo_sapiens_950up_50down_promoter_DEGS_up", "-p", OutputPAth, "-t",
+                                    "tab2fasta"]), shell=True)
+except:
+    print "Error during background parsing."
+else:
+    print "DEGS up parsing complete."
+# tab2fasta down
+try:
+    subprocess.check_call(" ".join(["python", conversionscript, "-i", OutputPAth + "promoter_DEGS_down.tab", "-o",
+                                    "Homo_sapiens_950up_50down_promoter_DEGS_down", "-p", OutputPAth, "-t",
+                                    "tab2fasta"]), shell=True)
+except:
+    print "Error during background parsing."
+else:
+    print "DEGS down parsing complete."
+
+os.chdir("/home/spuccio/pscan/Human_and_Mouse/Jaspar_MatricesHomo_sapiens/")
+
+# try:
+#     subprocess.check_call(" ".join([pscanpath,"-q",OutputPAth + "Homo_sapiens_950up_50down_promoter_DEGS_up_1_newid.fasta","-p",
+#                                     OutputPAth + "Homo_sapiens_950up_50down_promoter_expressed_1_newid.fasta"]),shell=True)
+# except:
+#     print "Error pscan analysis fail."
+# else:
+#     print "Analysis complete."
+#
+#
+# try:
+#     subprocess.check_call(" ".join([pscanpath,"-q",OutputPAth + "Homo_sapiens_950up_50down_promoter_DEGS_down_1_newid.fasta","-p",
+#                                     OutputPAth + "Homo_sapiens_950up_50down_promoter_expressed_1_newid.fasta"]),shell=True)
+# except:
+#     print "Error pscan analysis fail."
+# else:
+#     print "Analysis complete."
+#
+try:
+    subprocess.check_call(" ".join([pscanpath,"-q",OutputPAth + "Homo_sapiens_950up_50down_promoter_DEGS_up_1_newid.fasta","-p",
+                                    OutputPAth + "Homo_sapiens_950up_50down_promoter_DEGS_down_1_newid.fasta"]),shell=True)
+except:
+    print "Error pscan analysis fail."
+else:
+    print "Analysis complete."
+
+try:
+    subprocess.check_call(" ".join([pscanpath,"-q",OutputPAth + "Homo_sapiens_950up_50down_promoter_DEGS_down_1_newid.fasta","-p",
+                                    OutputPAth + "Homo_sapiens_950up_50down_promoter_DEGS_up_1_newid.fasta"]),shell=True)
+except:
+    print "Error pscan analysis fail."
+else:
+    print "Analysis complete."
