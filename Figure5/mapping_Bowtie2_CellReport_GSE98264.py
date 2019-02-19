a3845a53793202845f0b392a975886881754ce51
sinnamone <sinnamone@gmail.com>
2019-02-19 14:04:28 +0100

    Update

--- Figure5/mapping_Bowtie2_CellReport_GSE98264.py
+++ Figure5/mapping_Bowtie2_CellReport_GSE98264.py
@@ -2,12 +2,13 @@ import os
 import subprocess
 import sys
 # set path tool
-threads = "40"
+threads = "20"
 MAX_FRAG_LEN = "2000"
+indexname = "GRCm38"
 # the multimapping flag
 multimap = "4"
 # folder with Mus musculus (house mouse) genome assembly GRCm38 (mm10)
-GRCm38indexpath = "/home/spuccio/AnnotationBowtie2/Mus_musculus/GRCm38.p6"
+GRCm38indexpath = "/home/spuccio/AnnotationBowtie2/Mus_musculus/GRCm38.p6/"
 # bowtie2-build path
 bowtiebuild2path = "/home/spuccio/miniconda3/envs/chipseq_env/bin/bowtie2-build"
 # bowtie2 path
@@ -15,14 +16,15 @@ bowtie2path = "/home/spuccio/miniconda3/envs/chipseq_env/bin/bowtie2"
 # path folder
 projectdir = "/mnt/datadisk2/spuccio/SP011_Integration_ChipSeqGSE98264_RnaSeqSP010/"
 # path out file sam
-mappingout = "".join([projectdir, "mapping"])
+mappingout = "".join([projectdir, "bowtie2_mapping_GSE98264"])
 # input file path
-raw_data_dir = "".join([projectdir, "Raw_ReadsChipFigure5/"])
-#
+raw_data_dir = "".join([projectdir, "raw_data_GSE98264/"])
+# Path
 GRCm38fasta = "".join([GRCm38indexpath, "/GRCm38.primary_assembly.fa"])
-raw_fastq = {"Treg_Irf4_r1": ["Treg_Irf4_input_1.fastq", "Treg_Irf4_input_2.fastq" ],
-             "Treg_Irf4_r2": ["Treg_Irf4_input_3.fastq","Treg_Irf4_input_4.fastq"]}
-raw_fastq2 = {"Treg_Irf4_r3": "Treg_Irf4_input_5.fastq"}
+raw_fastq = {"Treg_Irf4_r1": ["SRR5483021_1.fastq", "SRR5483021_2.fastq"],
+             "Treg_Irf4_r2": ["SRR5483022_1.fastq", "SRR5483022_2.fastq"]}
+raw_fastq2 = {"Treg_Irf4_r3": "SRR5483020_1.fastq"}
+
 
 def createdir(dirpath):
     if not os.path.exists(dirpath):
@@ -35,20 +37,18 @@ def createdir(dirpath):
 
 
 def checkindex(indexpath, fastafile, indexname):
-    for i in range(len(os.listdir(indexpath))):
-        if os.listdir(indexpath)[i].split(".")[-1] == "bt2":
-            print("Genome index of Fasta file already exists.")
-            break
+    if os.path.isfile("".join([indexpath, indexname, ".1.bt2"])) == True:
+        print("Genome index of Fasta file already exists.")
+    else:
+        try:
+            os.chdir(GRCm38indexpath)
+            subprocess.check_call(" ".join([bowtiebuild2path, "--threads", threads, fastafile, indexname]),
+                                  shell=True)
+        except subprocess.CalledProcessError:
+            print("ERROR.Fastqc analysis failed. Stop execution.")
+            sys.exit(1)
         else:
-            try:
-                os.chdir(GRCm38indexpath)
-                subprocess.check_call(" ".join([bowtiebuild2path, "--threads", threads, fastafile, indexname]),
-                                      shell=True)
-            except subprocess.CalledProcessError:
-                print("ERROR.Fastqc analysis failed. Stop execution.")
-                sys.exit(1)
-            else:
-                print("Fastq analysis complete.")
+            print("Fastq analysis complete.")
     return indexname
 
 
@@ -82,7 +82,7 @@ def bowtie2mappingsinglend(indexname, fastqname, samname):
 
 
 if __name__ == "__main__":
-    index = checkindex(GRCm38indexpath, GRCm38fasta, "GRCm38")
+    index = checkindex(GRCm38indexpath, GRCm38fasta, indexname)
     createdir(mappingout)
     os.chdir(raw_data_dir)
     for key, value in raw_fastq2.items():
