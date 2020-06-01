13bebaccd54f14b1c3b05019c0fec0f6ac9d30ea
LugliLab <simone.puccio@humanitasresearch.it>
2020-06-01 18:12:58 +0200

    Update script_heatmap_scaled.R

--- Figure4/script_heatmap_scaled.R
+++ Figure4/script_heatmap_scaled.R
@@ -1,25 +1,16 @@
-#####Select the work directory (change the path to your files) 
+##### Select the work directory (change the path to your files) 
 setwd("INSERT HERE PATH")
-
-#####Load the gplots package
+##### Load the gplots package
 library(gplots)
-
-#####Load the txt file (tab delimited)
+##### Load the txt file (tab delimited)
 matrix_r<-read.table("heatmap_template.txt", sep="\t", stringsAsFactors=F, header=T, row.names=1)
-matrix_t<-t(matrix_r)
-
-#####Define the colors you want to use for the heatmap
+##### Define the colors you want to use for the heatmap
 heat.colors<-colorRampPalette(c("navy","blue4","blue","skyblue","khaki1","lightgoldenrod1","goldenrod1","orange"))(100)
-
-#####Make a heatmap  ##change parameters if needed (type help(heatmap.2) to get a full description of the options)
-data<-matrix_t
-scale.data<-as.matrix((data-apply(data,1,mean))/apply(data,1,sd))
-
-matrix_scaled<-scale.data
-matrix<-t(matrix_scaled)
-
-heatmap.2(as.matrix(matrix),
+##### Make a heatmap  ##change parameters if needed (type help(heatmap.2) to get a full description of the options)
+scale.data<-as.matrix((t(matrix_r)-apply(t(matrix_r),1,mean))/apply(t(matrix_r),1,sd))
+##### Plot heatmap 
+heatmap.2(as.matrix(t(scale.data)),
     dendrogram="both", scale="none",  na.color="grey",
-      col=heat.colors,trace="none",labRow = rownames(matrix),key = TRUE,keysize = 1, cexCol=1,
+      col=heat.colors,trace="none",labRow = rownames(t(scale.data)),key = TRUE,keysize = 1, cexCol=1,
       	 density.info="none",symkey=FALSE, margins=c(7,10), main="iMFI", 
       	 	xlab="Markers", ylab= "CLUSTERS")
